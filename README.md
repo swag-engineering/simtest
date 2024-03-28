e22da632cd325709a8e1f3284823d847fe7516d4
Anton <anton.rak@swag-engineering.io>
2024-03-28 15:47:44 +0100

    added worflow; fixed tests

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# Simweb backend
-
-This container is used to run the tests in most secure way, not to break the app and not to expose any sensitive data to
-the outer world.
-
-## Installation
-
-To set up local development environment, please refer
-to [Deployment manual](http://gitlab.private.swag-engineering.io/simweb/deployment#simweb-deployment).
+# Simtest
--- tests/integration/install_packages_test.py
+++ tests/integration/install_packages_test.py
@@ -4,13 +4,13 @@ import pytest
 from simtest import proto
 
 
-@pytest.mark.parametrize("package_name", ["beautifulsoup4", "requests", "httpie"], indirect=True)
+@pytest.mark.parametrize("package_name", ["beautifulsoup4", "clifashion", "pyella"], indirect=True)
 async def test_install_valid_packages(
         package_name: str,
         package_bytes: bytes,
         simtest_client: proto.grpc.SimtestStub
 ):
-    request = proto.grpc.InstallWheelPackageRequest(package_name, package_bytes)
+    request = proto.grpc.InstallWheelPackageRequest(package_name, package_bytes, True)
     await simtest_client.install_wheel_package(request)
 
 
@@ -20,7 +20,7 @@ async def test_install_invalid_packages(
         package_bytes: bytes,
         simtest_client: proto.grpc.SimtestStub
 ):
-    request = proto.grpc.InstallWheelPackageRequest("flask", package_bytes)
+    request = proto.grpc.InstallWheelPackageRequest("flask", package_bytes, True)
     with pytest.raises(grpclib.GRPCError) as exc_info:
         await simtest_client.install_wheel_package(request)
         assert exc_info.value.status == grpclib.const.Status.NOT_FOUND
